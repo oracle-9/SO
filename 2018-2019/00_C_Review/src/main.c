67a1675b69189ec9f682ca4c0aece5d63ed9cb8c
Oracle <oracle.uminho@gmail.com>
2019-05-27 21:44:22 +0100

    Use additional #defines in main.c

--- 2018-2019/00_C_Review/src/main.c
+++ 2018-2019/00_C_Review/src/main.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PERSON_NAME "Oracle"
-#define PERSON_AGE 791
+#define PERSON1_NAME "Oracle"
+#define PERSON1_AGE 791
+#define PERSON2_NAME "Roberto"
+#define PERSON2_AGE 800
 
 int main(void) {
-    Person p1 = person_new(PERSON_NAME, PERSON_AGE); /* needs freeing */
-    char *const name       = person_get_name(p1);    /* needs freeing */
+    Person p1 = person_new(PERSON1_NAME, PERSON1_AGE); /* needs freeing */
+    char *const name       = person_get_name(p1);      /* needs freeing */
     const unsigned int age = person_get_age(p1);
 
     printf("Person 1 {\n"
@@ -21,8 +23,8 @@ int main(void) {
     free(name);
     person_delete(p1);
 
-    person_set_name(&p2, "Roberto");
-    person_set_age(&p2, 800);
+    person_set_name(&p2, PERSON2_NAME);
+    person_set_age(&p2, PERSON2_AGE);
     printf("\nPerson 2 {\n"
            "    name: %s\n"
            "    age: %u\n"

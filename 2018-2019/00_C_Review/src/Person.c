95724458473720635b9f5e33972da2c1253d65d2
Oracle <oracle.uminho@gmail.com>
2019-06-01 00:46:50 +0100

    unsigned -> unsigned int; Remove minor code duplication

--- 2018-2019/00_C_Review/src/Person.c
+++ 2018-2019/00_C_Review/src/Person.c
@@ -1,3 +1,13 @@
+/*
+ * Description : University of Minho Operating Systems 2018/2019 worksheet 0
+ *               solved exercises.
+ *               Resolução da ficha 0 de Sistemas Operativos 2018/2019 da
+ *               Universidade do Minho.
+ * Copyright   : © Oracle, 2019
+ * License     : MIT
+ * Maintainer  : oracle.uminho@gmail.com
+ */
+
 #define _POSIX_C_SOURCE 200809L /* strndup() */
 
 #include "Person.h"
@@ -6,13 +16,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-Person person_new(const char *name, unsigned age) {
+Person person_new(const char *name, unsigned int age) {
     return (Person){ .name = strndup(name, MAX_NAME_LEN), .age = age };
 }
 
-Person person_copy(const Person p) {
-    return (Person){ .name = strndup(p.name, MAX_NAME_LEN), .age = p.age };
-}
+Person person_copy(const Person p) { return person_new(p.name, p.age); }
 
 void person_delete(Person p) { free(p.name); }
 
